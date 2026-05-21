d21132ddde027ce9bb9f4ed540e7a2b9fd2b95a4
Nina Le <ninal11@uci.edu>
2026-05-21 20:36:53 +0000

    generate_report() prints report to m2_report.txt

--- search.py
+++ search.py
@@ -11,6 +11,7 @@ stemmer = PorterStemmer()
 
 INDEX_FILE = "inverted_index.json"
 DEV_FOLDER = "DEV"
+REPORT = "m2_report.txt"
 
 def build_url_map(root_path=DEV_FOLDER):
     doc_id_to_url = {}
@@ -61,6 +62,17 @@ def boolean_and_search(query_terms, index):
 def generate_report():
     with open(INDEX_FILE, 'r') as f:
         inverted_index = json.load(f)
+
+    url_map = build_url_map()
+    queries = ["cristina lopes", "machine learning", "ACM", "master of software engineering"]
+
+    with open(REPORT, "w") as r:
+        for query in queries:
+            result = boolean_and_search(process_query(query), inverted_index)
+            print(f"\nQuery: {query}", file=r)
+            print(f"Top 5 URLs:", file=r)
+            for doc_id in result:
+                print(url_map[doc_id], file=r)
 
 if __name__ == "__main__":
     generate_report()
