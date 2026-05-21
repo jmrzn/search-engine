bd17ef72c7ca8e42b4978278e2b52a0f446218e9
emilyyng <emlyyang276@gmail.com>
2026-05-21 15:36:36 -0700

    commented generate_report() version using console
    the commented part uses the console to ask for queries instead of the set list of inputs, must uncomment this part and comment out lines 69-75 to use

--- search.py
+++ search.py
@@ -73,6 +73,19 @@ def generate_report():
             print(f"Top 5 URLs:", file=r)
             for doc_id in result[:5]:
                 print(url_map[doc_id], file=r)
+                
+    # version that uses console input for queries rather than a set list
+    # with open(REPORT, "w") as r:
+    #     while True:
+    #         query = input("Enter a search query or type 'q' to quit: ").strip().lower()
+    #         if (query == "q" or query == "quit"):
+    #             break
+    #         result = boolean_and_search(process_query(query), inverted_index)
+    #         print(f"\nQuery: {query}", file=r)
+    #         print(f"Top 5 URLs:", file=r)
+    #         for doc_id in result[:5]:
+    #             print(url_map[doc_id], file=r)
+    #         r.flush() # write buffered content to report immediately
 
 if __name__ == "__main__":
     generate_report()
