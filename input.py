2f4e1c7bedc7b21b6bc5f51a65e8e77329d73c50
emilyyng <emlyyang276@gmail.com>
2026-06-03 11:38:40 -0700

    updated to reflect newest state of search.py

--- input.py
+++ input.py
@@ -1,29 +1,43 @@
 from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 import json, math
-from search import process_query, boolean_and_search, rank_by_tfidf, compute_idf, build_url_map, INDEX_FILE
+from datetime import datetime
+from search import process_query, create_query_index, boolean_and_search, rank_by_tfidf, compute_idf, build_url_map, OFFSETS_FILE, DOC_LENGTHS_FILE
+
+# happens once before flask runs the app to search
+with open(OFFSETS_FILE, 'r') as f:
+    offsets = json.load(f)
+with open(DOC_LENGTHS_FILE, 'r') as d:
+    doc_lengths = json.load(d)
+    doc_lengths = {int(docID): length for docID, length in doc_lengths.items()}
+url_map = build_url_map()
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/search")
 def search_route():
-    with open(INDEX_FILE, 'r') as f:
-        inverted_index = json.load(f)
-
-    url_map = build_url_map()
-    idf = compute_idf(inverted_index, len(url_map))
-
-    query = request.args.get("q", "").strip()
+    # gets the query (stuff following ?q=) from the html
+    query = request.args.get("q", "").strip().lower()
     print(query)
     if not query:
         return jsonify({"urls": []})
 
+    start_time = datetime.now()
+    
     query_terms = process_query(query)
-    result = boolean_and_search(query_terms, inverted_index)
-    ranked_result = rank_by_tfidf(result, query_terms, inverted_index, idf)
-
-    urls = [url_map[doc_id] for doc_id, _ in ranked_result[:5]]
+    query_index = create_query_index(query_terms, offsets)
+    
+    idf = compute_idf(query_index, len(url_map))
+    result = boolean_and_search(query_terms, query_index)
+    ranked_result = rank_by_tfidf(result, query_terms, query_index, idf, doc_lengths)
+    
+    end_time = datetime.now()
+    time_diff = (end_time - start_time).total_seconds() * 1000
+    print("Search engine took", time_diff, "ms")
+    print(f"\nQuery: {query}")
+    
+    urls = [url_map[doc_id] for doc_id, score in ranked_result[:5]]
     return jsonify({"urls": urls})
 
 @app.route("/")
